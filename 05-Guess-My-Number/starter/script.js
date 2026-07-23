f5f2a06216681d5fdcfa334861af49bfc1045b2e
Ede-Rojin-Delibas <ederojind@gmail.com>
2026-07-23 17:56:11 +0300

    Worked on DOM events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,20 +1,15 @@
 'use strict';
 
-/* console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
 console.log(document.querySelector('.message').textContent);
-document.querySelector('.highscore').textContent = 48;
-document.querySelector('.number').textContent = 1;
+document.querySelector('.message').textContent = 'Good JOB, Correct Number! ';
+document.querySelector('.score').textContent = 10;
+document.querySelector('.number').textContent = 50;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
- */
-
-document.querySelector('.check').addEventListener('click', function () {
+/* document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔No Number';
   }
-});
+}); */
