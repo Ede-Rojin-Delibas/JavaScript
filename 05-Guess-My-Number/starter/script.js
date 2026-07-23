619f0fb56e1cd74f801f9bc8eb853869d06e2668
Ede-Rojin-Delibas <ederojind@gmail.com>
2026-07-23 17:57:09 +0300

    Used EventListeners on DOM Manipulations

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,6 +5,7 @@ document.querySelector('.message').textContent = 'Good JOB, Correct Number! ';
 document.querySelector('.score').textContent = 10;
 document.querySelector('.number').textContent = 50;
 
+document.querySelector('.guess').value = 23;
 /* document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
